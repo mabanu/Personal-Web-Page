d1bd4e595848eb0bebddb237e767514477b12e28
Maximiliano Barrientos <elbodysathba@gmail.com>
2023-01-13 13:16:25 +0100

    change colmena photos for personal fotos

--- src/components/Colmena.js
+++ src/components/Colmena.js
@@ -1,15 +1,22 @@
 import './Colmena.css';
+import beach from '../nikonD80/beach.jpg';
+import betterSunset from '../nikonD80/better_sunset.jpg';
+import botesBig from '../nikonD80/botes_big.jpg';
+import dulius from '../nikonD80/dulius.jpg';
+import Finisterre from '../nikonD80/Finisterre.jpg';
+import grass from '../nikonD80/grass.jpg';
+import montanas from '../nikonD80/montanas.jpg';
 
 function Colmena() {
   return (
     <div className="colmena-gallery">
-      <img src="https://picsum.photos/id/1040/300/300" alt="a house on a mountain" />
-      <img src="https://picsum.photos/id/106/300/300" alt="sime pink flowers" />
-      <img src="https://picsum.photos/id/136/300/300" alt="big rocks with some trees" />
-      <img src="https://picsum.photos/id/1039/300/300" alt="a waterfall, a lot of tree and a great view from the sky" />
-      <img src="https://picsum.photos/id/110/300/300" alt="a cool landscape" />
-      <img src="https://picsum.photos/id/1047/300/300" alt="inside a town between two big buildings" />
-      <img src="https://picsum.photos/id/1057/300/300" alt="a great view of the sea above the mountain" />
+      <img src={beach} alt="a house on a mountain" />
+      <img src={betterSunset} alt="sime pink flowers" />
+      <img src={botesBig} alt="big rocks with some trees" />
+      <img src={dulius} alt="a waterfall, a lot of tree and a great view from the sky" />
+      <img src={Finisterre} alt="a cool landscape" />
+      <img src={grass} alt="inside a town between two big buildings" />
+      <img src={montanas} alt="a great view of the sea above the mountain" />
     </div>
   );
 }
